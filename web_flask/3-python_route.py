14291316d0e7f58c1960ca3c9a44456bcd812305
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-10-21 19:21:25 +0000

    task answer

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,6 +22,7 @@ def c_variable(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 def python_variable(text='is cool'):
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
